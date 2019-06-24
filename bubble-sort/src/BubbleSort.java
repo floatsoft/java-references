b28dc2a567d6f650b97b39d16991c13d3974a05b
Kevin Gonzalez <typekev@gmail.com>
2019-06-24 18:43:56 +0200

    Documentation of int[] declaration

--- bubble-sort/src/BubbleSort.java
+++ bubble-sort/src/BubbleSort.java
@@ -16,21 +16,19 @@ import java.util.Arrays;
 public class BubbleSort {
 
     /**
-     * The Java programming language is statically-typed,
-     * which means that all variables must first be declared before they can be used.
-     * See https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
-     *
-     * We declare a variable (or field) of the type int[] named unsortedNumbersList.
-     * See https://github.com/floatsoft/java-references/blob/master/hello-world/src/HelloWorld.java#L1-L18
-     *
-     * We declare unsortedNumbersList with the private access modifier.
+     * We declare a static variable named unsortedNumbersList, with the private access modifier.
+     * See https://github.com/floatsoft/java-references/blob/hello-world/hello-world/src/VariableDeclaration.java#L8-L28
      * See https://github.com/floatsoft/java-references/blob/master/hello-world/src/HelloWorld.java#L6-L9
      *
-     * Class variables or static fields are variables declared with the static modifier.
-     * There is only one copy of a static field and it belongs to the class rather than an instance of that class (an object).
-     * A variable that is not declared as static is created only when the class is instantiated as an object.
-     * therefore in order to use unsortedNumbersList as a variable in our main method, it must be declared static.
-     * See https://docs.oracle.com/javase/tutorial/java/javaOO/classvars.html
+     * unsortedNumbersList is declared with the type int[], meaning that it is an array containing values of the
+     * primitive type int.
+     * The [] may appear as part of the type at the beginning of the declaration, or as part of the declarator
+     * for a particular variable, or both.
+     * See https://github.com/floatsoft/java-references/blob/hello-world/hello-world/src/VariableDeclaration.java#L11
+     * See https://docs.oracle.com/javase/specs/jls/se7/html/jls-10.html#jls-10.2
+     *
+     * unsortedNumbersList is a one-dimensional array.
+     * See https://en.wikipedia.org/wiki/Array_data_structure#One-dimensional_arrays
      */
     private static int[] unsortedNumbersList = {8, 5, 6, 9, 3, 1, 4, 2, 7, 10};
 
